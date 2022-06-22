46bfecbf8415fe5df341de4a9ad414805b53e0f5
Orkhan Farajov <orkhankhf@gmail.com>
2022-06-22 22:22:08 +0300

    Pin detector for windows

---
+++ PinFinder/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        const UInt32 WM_KEYDOWN = 0x0100;
+        const int VK_F5 = 0x74;
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+        [STAThread]
+        static void Main()
+        {
+            Thread.Sleep(4000);
+            for (Int32 index = 2873; index < 2875; index++)
+            {
+                InputSimulator sim = new InputSimulator();
+                sim.Keyboard.TextEntry("runas /user:Developer \"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"");
+                Thread.Sleep(5);
+                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                Thread.Sleep(5);
+                sim.Keyboard.TextEntry(index.ToString("0000"));
+                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
---
+++ PinFinder/PinFinder/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+namespace PinFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Thread.Sleep(1000);
+            VirtualMouse.LeftMouseClick(300, 450);
+        }
+    }
+}
